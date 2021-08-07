2371f7761c2ce83b916b0bac168486361dd1d819
FatOtter <narcy188@gmail.com>
2021-08-07 14:41:56 +0800

    Added support for CUDA

--- clean_version/package_tester.py
+++ clean_version/package_tester.py
@@ -96,5 +96,7 @@ class PackageTester:
 
 
 if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
     test = PackageTester()
     test.draw_landscape()
